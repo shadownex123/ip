27262b6097a8a7e136c2abd3be8068013f2c2587
shadownex123 <64712599+shadownex123@users.noreply.github.com>
2026-06-20 15:53:13 +0330

    Update update_sub.py

--- update_sub.py
+++ update_sub.py
@@ -2,7 +2,9 @@ import base64
 import requests
 
 ips_url = "https://raw.githubusercontent.com/rqzbeh/rqzbeh-wp/refs/heads/main/clean-ips.json"
-base_config = "vless://9ba99b6f-c7a4-44da-a298-d0e883a4b40e@{ip}:443?mode=auto&path=%2F&security=tls&alpn=h2%2Chttp%2F1.1&encryption=none&extra=%7B%22xPaddingBytes%22%3A%22100-1000%22%7D&insecure=0&host=ezvs.ir&fp=random&type=xhttp&allowInsecure=0&sni=ezvs.ir#0-1#ReVeCiTy-{index}"
+
+# کانفیگ جدید شما
+base_config = "vless://9ba99b6f-c7a4-44da-a298-d0e883a4b40e@{ip}:443?mode=auto&path=%2F&security=tls&alpn=h2%2Chttp%2F1.1&encryption=none&extra=%7B%22xPaddingBytes%22%3A%22100-1000%22%7D&insecure=0&host=ezvs.ir&fp=random&type=xhttp&allowInsecure=0&sni=ezvs.ir#ReVeCiTy-{index}"
 
 ips = requests.get(ips_url).json()
 
